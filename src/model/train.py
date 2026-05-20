a8cc1ccc9e8c329ff56754e31fb9cafb7c1e977c
Ivan Kirchanov <kirchanov25@gmail.com>
2026-05-20 16:34:27 +0300

    Fix linting: add split_data function and fix code style

--- src/model/train.py
+++ src/model/train.py
@@ -1,18 +1,16 @@
 # Import libraries
-
 import argparse
 import glob
 import os
 
 import pandas as pd
-
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
 # define functions
 def main(args):
     # TO DO: enable autologging
-
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -33,12 +31,18 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
+def split_data(df):
+    X = df.drop('Diabetic', axis=1)
+    y = df['Diabetic']
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.30, random_state=0)
+    return X_train, X_test, y_train, y_test
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(
+        X_train, y_train)
 
 
 def parse_args():
@@ -56,6 +60,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
